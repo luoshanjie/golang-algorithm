3243ba8adab619972f6561f513289985272d26df
luoshanjie <luoshanjie@gmail.com>
2022-06-14 23:59:37 +0800

    update code format

--- internal/sort/bubble/bubble.go
+++ internal/sort/bubble/bubble.go
@@ -1,11 +1,11 @@
 package bubble
 
 func sort(arr []int) {
-	for j := len(arr) - 1; j >= 1; j-- {
-		for i := 1; i <= j; i++ {
-			if arr[i-1] > arr[i] {
-				arr[i-1], arr[i] = arr[i], arr[i-1]
-			}
-		}
-	}
+    for j := len(arr) - 1; j >= 1; j-- {
+        for i := 1; i <= j; i++ {
+            if arr[i-1] > arr[i] {
+                arr[i-1], arr[i] = arr[i], arr[i-1]
+            }
+        }
+    }
 }
--- internal/sort/bubble/bubble_test.go
+++ internal/sort/bubble/bubble_test.go
@@ -1,17 +1,17 @@
 package bubble
 
 import (
-	"testing"
+    "testing"
 )
 
 func TestSort(t *testing.T) {
-	arr := []int{1, 3, 2, 7, 5, 8, 10, 9, 6, 4}
-	sort(arr)
-	for i := 1; i <= 10; i++ {
-		get := arr[i-1]
-		want := i
-		if get != want {
-			t.Errorf("get %d but want %d\n", get, want)
-		}
-	}
+    arr := []int{1, 3, 2, 7, 5, 8, 10, 9, 6, 4}
+    sort(arr)
+    for i := 1; i <= 10; i++ {
+        get := arr[i-1]
+        want := i
+        if get != want {
+            t.Errorf("get %d but want %d\n", get, want)
+        }
+    }
 }
