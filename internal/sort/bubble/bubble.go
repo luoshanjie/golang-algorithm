7a83d68876bb02e04923b2dcfc65a13ff2f714a7
luoshanjie <luoshanjie@gmail.com>
2022-06-14 23:53:05 +0800

    add bubble sort

--- internal/sort/bubble/bubble.go
+++ internal/sort/bubble/bubble.go
@@ -1,5 +1,11 @@
 package bubble
 
 func sort(arr []int) {
-
+	for j := len(arr) - 1; j >= 1; j-- {
+		for i := 1; i <= j; i++ {
+			if arr[i-1] > arr[i] {
+				arr[i-1], arr[i] = arr[i], arr[i-1]
+			}
+		}
+	}
 }
